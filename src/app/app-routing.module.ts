fa14d818d3dd14929029684f1b4a0be8fffcc9ef
LL\ghakoli <ghanahshyam.koli@publicissapient.com>
2021-05-08 17:53:34 +0530

    change home routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'user'},
+  {path: '', pathMatch: 'full', redirectTo: 'products'},
   {path: 'user', loadChildren: () => import('./modules/login-register/login-register.module').then((m)=> m.LoginRegisterModule) },
   {path: 'cart', loadChildren: () => import('./modules/cart/cart.module').then((m)=> m.CartModule) },
   {path: 'products', loadChildren: () => import('./modules/products/products.module').then((m)=> m.ProductsModule) },
